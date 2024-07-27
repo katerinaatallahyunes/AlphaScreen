c6f30d3488f09f418dd4dfe872e4040affc3fba5
katerinaatallahyunes <147754368+katerinaatallahyunes@users.noreply.github.com>
2024-07-27 19:12:01 -0400

    Add files via upload

---
+++ preprocessing/script_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=
+#SBATCH --cpus-per-task=
+#SBATCH --partition=gpu
+#SBATCH --time=
+#SBATCH --gres=
+#SBATCH --mem=
+#SBATCH --mail-user=
+#SBATCH --mail-type=
+
+module load 
+
+run_singularity \
+--model_preset= \
+--fasta_paths= \
+--max_template_date= \
+--use_precomputed_msas=
+--output_dir= \
+--num_multimer_predictions_per_model=
